4c78d2711037aef9c62a4964d2a7b1688faa57b6
nihal <rahmannihal72@gmail.com>
2025-03-15 11:26:57 +0530

    typescript-fix

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-const express = require("express");
+import express from "express";
 const io = require("socket.io")(4001, {
 	cors: { origin: "*" },
 });
 const app = express();
+
+app.use(express.json());
 const port = 8080;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
-});
-app.get("/", (req, res) => {
-	res.send("Hello");
 });
 
 io.on("connection", (socket) => {
